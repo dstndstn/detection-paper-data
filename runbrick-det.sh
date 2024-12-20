5e0456dc07ce58b4a16eec38db287b4d1223c978
Dustin Lang <dstndstn@gmail.com>
2024-12-20 13:15:35 -0500

    add full CCDs file from DR11, and runbrick script to generate it

---
+++ runbrick-det.sh
@@ -0,0 +1,120 @@
+#! /bin/bash
+
+# Script for running the legacypipe code within a Shifter container at NERSC
+
+export COSMO=/dvs_ro/cfs/cdirs/cosmo
+
+export LEGACY_SURVEY_DIR=$COSMO/work/legacysurvey/dr11
+#export LEGACY_SURVEY_DIR=/global/homes/d/dstn/detection-paper/survey-dir
+outdir=$SCRATCH/detection
+
+export GAIA_CAT_DIR=$COSMO/data/gaia/dr3/healpix
+export GAIA_CAT_PREFIX=healpix
+export GAIA_CAT_SCHEME=nested
+export GAIA_CAT_VER=3
+
+export DUST_DIR=$COSMO/data/dust/v0_1
+export UNWISE_COADDS_DIR=$COSMO/data/unwise/neo7/unwise-coadds/fulldepth:$COSMO/data/unwise/allwise/unwise-coadds/fulldepth
+export UNWISE_COADDS_TIMERESOLVED_DIR=$COSMO/work/wise/outputs/merge/neo7
+export UNWISE_MODEL_SKY_DIR=$COSMO/data/unwise/neo7/unwise-catalog/mod
+
+export TYCHO2_KD_DIR=$COSMO/staging/tycho2
+export LARGEGALAXIES_CAT=$COSMO/staging/largegalaxies/v3.0/SGA-ellipse-v3.0.kd.fits
+export SKY_TEMPLATE_DIR=$COSMO/work/legacysurvey/dr10/calib/sky_pattern
+
+unset BLOB_MASK_DIR
+unset PS1CAT_DIR
+unset GALEX_DIR
+
+# Don't add ~/.local/ to Python's sys.path
+export PYTHONNOUSERSITE=1
+# Force MKL single-threaded
+# https://software.intel.com/en-us/articles/using-threaded-intel-mkl-in-multi-thread-application
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+# To avoid problems with MPI and Python multiprocessing
+export MPICH_GNI_FORK_MODE=FULLCOPY
+export KMP_AFFINITY=disabled
+
+# brick="$1"
+# # strip whitespace from front and back
+# #brick="${brick#"${brick%%[![:space:]]*}"}"
+# #brick="${brick%"${brick##*[![:space:]]}"}"
+# bri=${brick:0:3}
+# 
+# mkdir -p "$outdir/logs/$bri"
+# mkdir -p "$outdir/metrics/$bri"
+# mkdir -p "$outdir/pickles/$bri"
+# log="$outdir/logs/$bri/$brick.log"
+log="$outdir/log"
+echo Logging to: "$log"
+#echo Running on $(hostname)
+
+# # Config directory nonsense
+export TMPCACHE=$(mktemp -d)
+mkdir $TMPCACHE/cache
+mkdir $TMPCACHE/config
+# astropy
+export XDG_CACHE_HOME=$TMPCACHE/cache
+export XDG_CONFIG_HOME=$TMPCACHE/config
+mkdir $XDG_CACHE_HOME/astropy
+cp -r $HOME/.astropy/cache $XDG_CACHE_HOME/astropy
+mkdir $XDG_CONFIG_HOME/astropy
+cp -r $HOME/.astropy/config $XDG_CONFIG_HOME/astropy
+# matplotlib
+export MPLCONFIGDIR=$TMPCACHE/matplotlib
+mkdir $MPLCONFIGDIR
+cp -r $HOME/.config/matplotlib $MPLCONFIGDIR
+
+echo -e "\n\n\n" >> "$log"
+echo "-----------------------------------------------------------------------------------------" >> "$log"
+echo -e "\nStarting on $(hostname)\n" >> "$log"
+echo "-----------------------------------------------------------------------------------------" >> "$log"
+
+export LEGACYPIPE_DIR=.
+export PYTHONPATH=.:${PYTHONPATH}
+#     --skip-calibs \
+
+python -u -O $LEGACYPIPE_DIR/legacypipe/runbrick.py \
+       --force-all \
+       --radec 36.45 -4.6 \
+       --width 4000 --height 4400 \
+     --skip \
+     --bands g,r,i \
+     --rgb-stretch 1.5 \
+     --nsatur 2 \
+     --survey-dir "$LEGACY_SURVEY_DIR" \
+      --outdir "$outdir" \
+     --checkpoint "${outdir}/checkpoints/${bri}/checkpoint-${brick}.pickle" \
+     --checkpoint-period 120 \
+     --pickle "${outdir}/pickles/${bri}/runbrick-%(brick)s-%%(stage)s.pickle" \
+     --release 11111 \
+     --threads 32 \
+     --stage image_coadds --blob-image \
+      >> "$log" 2>&1
+
+#     --write-stage srcs \
+#     --stage image_coadds \
+#     --bands g,r,i,z \
+#    --cache-dir "$CACHE_DIR" \
+
+#     --cache-outliers \
+#     --max-memory-gb 20 \
+# --no-wise-ceres helps for very dense fields.
+#     --no-wise-ceres \
+#     --write-stage coadds \
+#     --write-stage wise_forced \
+#     --run south \
+#     --ps "${outdir}/metrics/${bri}/ps-${brick}-${SLURM_JOB_ID}.fits" \
+#     --ps-t0
+
+# Save the return value from the python command -- otherwise we
+# exit 0 because the rm succeeds!
+status=$?
+
+# /Config directory nonsense
+rm -R $TMPCACHE
+
+exit $status
+
+# QDO_BATCH_PROFILE=cori-shifter qdo launch -v tst 1 --cores_per_worker 8 --walltime=30:00 --batchqueue=debug --keep_env --batchopts "--image=docker:dstndstn/legacypipe:intel" --script "/src/legacypipe/bin/runbrick-shifter.sh"
